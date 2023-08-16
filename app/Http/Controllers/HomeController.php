e6d05e9a129ed8e43400d13c9763d71bc4fd9ab5
Miguel Angel Calvache Giraldo <calvache116@gmail.com>
2023-08-16 10:55:02 -0500

    Tutorial 2 terminado

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\View\View;
+
 class HomeController extends Controller
 {
     public function index(): View
--- app/Http/Controllers/Productcontroller.php
+++ app/Http/Controllers/Productcontroller.php
@@ -2,64 +2,56 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Product;
+use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
 use Illuminate\View\View;
-use Illuminate\Http\RedirectResponse;
 
 class ProductController extends Controller
 {
-    public static $products = [
-        ["id"=>"1", "name"=>"TV", "description"=>"Best TV", "price"=>"1500"],
-        ["id"=>"2", "name"=>"iPhone", "description"=>"Best iPhone", "price"=>"2000"],
-        ["id"=>"3", "name"=>"Chromecast", "description"=>"Best Chromecast", "price"=>"100"],
-        ["id"=>"4", "name"=>"Glasses", "description"=>"Best Glasses", "price"=>"30"]
-    ];
-
     public function index(): View
     {
         $viewData = [];
-        $viewData["title"] = "Products - Online Store";
-        $viewData["subtitle"] =  "List of products";
-        $viewData["products"] = ProductController::$products;
-        return view('product.index')->with("viewData", $viewData);
+        $viewData['title'] = 'Products - Online Store';
+        $viewData['subtitle'] = 'List of products';
+        $viewData['products'] = Product::all();
+
+        return view('product.index')->with('viewData', $viewData);
     }
 
-    public function show(string $id) : View | RedirectResponse
+    public function show(string $id): View|RedirectResponse
     {
-        if (!isset(ProductController::$products[$id-1])) {
-            return redirect()->route("home.index");
+
+        $product = Product::findOrFail($id);
+
+        if ($product['price'] > 100) { // Revisar
+            $viewData['color'] = 'red';
         }
 
-        // If the price of a product is greater than 100, display the product name in red
-        $viewData = [];
-        $viewData["color"] = "black";
-        if (ProductController::$products[$id-1]["price"] > 100) {
-            $viewData["color"] = "red";
-        }
-        $product = ProductController::$products[$id-1];
-        $viewData["title"] = $product["name"]." - Online Store";
-        $viewData["subtitle"] =  $product["name"]." - Product information";
-        $viewData["product"] = $product;
-        
-        return view('product.show')->with("viewData", $viewData);
+        $viewData['title'] = $product['name'].' - Online Store';
+        $viewData['subtitle'] = $product['name'].' - Product information';
+        $viewData['product'] = $product;
+
+        return view('product.show')->with('viewData', $viewData);
     }
 
-    public function create(): View 
+    public function create(): View
     {
-    $viewData = []; //to be sent to the view
-    $viewData["title"] = "Create product";
+        $viewData = []; //to be sent to the view
+        $viewData['title'] = 'Create product';
 
-    return view('product.create')->with("viewData",$viewData);
-    
-}
+        return view('product.create')->with('viewData', $viewData);
 
-    public function save(Request $request)
+    }
+
+    public function save(Request $request): RedirectResponse
     {
         $request->validate([
-            "name" => "required",
-            "price" => "required|numeric|gt:100",
+            'name' => 'required',
+            'price' => 'required|numeric|gt:100',
         ]);
-        dd($request->all());
-        //here will be the code to call the model and save it to the database
+        Product::create($request->only(['name', 'price']));
+
+        return back();
     }
 }
